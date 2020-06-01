69d137ed52c0f90f14c282abc4d7597c94443f00
sohamsshah <sohamshah456@gmail.com>
2020-06-01 18:43:09 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Header()
+{
+    return <header>
+        <h1> TechPad</h1>
+        </header>
+}
+
+export default Header; 
---
+++ src/Components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Note()
+{
+    return <div class="note">
+        <h1>This is the Title</h1>
+        <p> Hello</p>
+        </div>
+        
+}
+
+export default Note; 
